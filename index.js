b34ea36bd18762c6dac352ee6b3dce406e18c7c2
Kiichiro Kondo <kiichiro@Kiichiros-MacBook-Air.local>
2024-07-09 01:27:35 +0900

    fin

---
+++ index.js
@@ -0,0 +1,11 @@
+//  const payload=window.location.hash.substring(1)
+//  location.replace(payload);
+//#javascript:alert(1) network型
+
+  const payload1=window.location.hash.substring(1)
+  eval(payload1);
+ //#jalert(1)　vm型
+
+//const payload2 = window.location.hash.substring(1);
+//document.querySelector("body").innerHTML = decodeURIComponent(payload2);
+//#<img src="" onerror="alert(1)"> dom型
